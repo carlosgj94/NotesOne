295068e5a8bd65991a0072b226c09671900bb1ed
Carlos Gonzalez <carlosgj94@gmail.com>
2017-12-28 16:04:29 +0100

    Initial Commit

---
+++ NotesOne WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  NotesOne WatchKit Extension
+//
+//  Created by Carlos González Juárez on 28/12/2017.
+//  Copyright © 2017 Carlos González Juárez. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
